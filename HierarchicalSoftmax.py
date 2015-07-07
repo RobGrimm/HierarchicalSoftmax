e0b1172902a61cab3a2db9c829693af408570ed5
RobertG <robb.grimm@gmail.com>
2015-07-07 12:23:22 +0200

    commit 2

--- HierarchicalSoftmax.py
+++ HierarchicalSoftmax.py
@@ -28,20 +28,20 @@ class HierarchicalSoftmax(object):
         # output layer is a 2-level binary tree
         # a predicted class label is defined as a a fixed arbitrary path through this tree
         # we thus need sqrt(n_out) nodes in the first level of the tree
-        self.n_layer1_nodes = numpy.ceil(numpy.sqrt(n_out)).astype('int64')
+        self.n_level1_nodes = numpy.ceil(numpy.sqrt(n_out)).astype('int64')
         # and at most sqrt(n_out) nodes in the second level
-        self.n_layer2_nodes = numpy.ceil(n_out/float(self.n_layer1_nodes)).astype('int64')
+        self.n_level2_nodes = numpy.ceil(n_out/float(self.n_level1_nodes)).astype('int64')
 
         # define weight matrix 'W' and bias 'b' for first layer in output tree
-        self.W = theano.shared(value=numpy.zeros((n_in,  self.n_layer1_nodes), dtype=theano.config.floatX),
+        self.W = theano.shared(value=numpy.zeros((n_in,  self.n_level1_nodes), dtype=theano.config.floatX),
                                name='W', borrow=True)
-        self.b = theano.shared(value=numpy.zeros((self.n_layer1_nodes,), dtype=theano.config.floatX),
+        self.b = theano.shared(value=numpy.zeros((self.n_level1_nodes,), dtype=theano.config.floatX),
                                name='b', borrow=True)
 
          # define weight matrix 'U' and bias 'c' for second layer in output tree
-        self.U = theano.shared(value=numpy.zeros((n_in,  self.n_layer2_nodes), dtype=theano.config.floatX),
+        self.U = theano.shared(value=numpy.zeros((n_in,  self.n_level2_nodes), dtype=theano.config.floatX),
                                name='W', borrow=True)
-        self.c = theano.shared(value=numpy.zeros((self.n_layer2_nodes,), dtype=theano.config.floatX),
+        self.c = theano.shared(value=numpy.zeros((self.n_level2_nodes,), dtype=theano.config.floatX),
                                name='b', borrow=True)
 
         self.params = [self.W, self.b, self.U, self.c]
@@ -76,8 +76,8 @@ class HierarchicalSoftmax(object):
             level2_vals = T.nnet.softmax(T.dot(input_, self.U) + self.c)
 
             # to each class label, assign a pair of nodes in layer1 and layer2 of the output tree
-            level1_idx = target // self.n_layer1_nodes
-            level2_idx = target % self.n_layer2_nodes
+            level1_idx = target // self.n_level1_nodes
+            level2_idx = target % self.n_level2_nodes
 
             # calculate cost of taking correct path through tree to
             bs = input_.shape[0]
