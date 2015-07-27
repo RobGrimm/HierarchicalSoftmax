3d16f4548333f307a2b82c4ccf473c7f3a351a75
me <hirse@gmx.net>
2015-07-27 19:50:56 +0200

    now works with any batch size (previously required BS=1 at tetst time)

--- HierarchicalSoftmax.py
+++ HierarchicalSoftmax.py
@@ -27,19 +27,20 @@ class HierarchicalSoftmax(object):
 
         # output layer is a 2-level graph
         # a predicted class label is defined as a a fixed arbitrary path through this graph
-        # we thus need sqrt(n_out) nodes in the first level
+        # we thus need at least sqrt(n_out) nodes in the first level
+        # (ceil of the scalar x is the smallest integer i, such that i >= x)
         self.n_level1_nodes = numpy.ceil(numpy.sqrt(n_out)).astype('int64')
-        # and at most sqrt(n_out) nodes in the second level -- note that we may sometimes have have more
-        # possible paths through the graph than there are output classes
+        # and at least sqrt(n_out) nodes in the second level -- note that sometimes we may end up
+        # with a graph that has a few more possible paths than there are output classes
         self.n_level2_nodes = numpy.ceil(n_out/float(self.n_level1_nodes)).astype('int64')
 
-        # define weight matrix 'W1' and bias 'b1' for first layer in output tree
+        # define weight matrix 'W1' and bias 'b1' for first level in output graph
         self.W1 = theano.shared(value=numpy.zeros((n_in,  self.n_level1_nodes), dtype=theano.config.floatX),
                                 name='W1', borrow=True)
         self.b1 = theano.shared(value=numpy.zeros((self.n_level1_nodes,), dtype=theano.config.floatX),
                                 name='b1', borrow=True)
 
-         # define weight matrix 'W2' and bias 'b2' for second layer in output tree
+         # define weight matrix 'W2' and bias 'b2' for second level in output graph
         self.W2 = theano.shared(value=numpy.zeros((n_in,  self.n_level2_nodes), dtype=theano.config.floatX),
                                 name='W2', borrow=True)
         self.b2 = theano.shared(value=numpy.zeros((self.n_level2_nodes,), dtype=theano.config.floatX),
@@ -82,11 +83,11 @@ class HierarchicalSoftmax(object):
 
         # compute only batch_size predictions [ time complexity is O(2 x sqrt(n_out)) = O(sqrt(n_out)) ]
         else:
-            # to each class label, assign a pair of nodes in layer1 and layer2 of the output tree
+            # to each class label, assign a pair of nodes in layer1 and layer2 of the output graph
             level1_idx = target // self.n_level1_nodes
             level2_idx = target % self.n_level2_nodes
 
-            # calculate cost of taking correct path through tree to
+            # calculate probability of taking correct path through the graph
             level1_val = level1_vals[T.arange(batch_size), level1_idx]
             level2_val = level2_vals[T.arange(batch_size), level2_idx]
             output_ = level1_val * level2_val
