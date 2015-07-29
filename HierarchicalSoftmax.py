d855cc3f21090d7dff7660f6c499fdf82d29662b
me <hirse@gmx.net>
2015-07-29 11:23:46 +0200

    now return (batch_size, input_size) matrix of predicted probabilities also during training

--- HierarchicalSoftmax.py
+++ HierarchicalSoftmax.py
@@ -56,7 +56,7 @@ class HierarchicalSoftmax(object):
         return T.argmax(self.forward_prop(input_), axis=1)
 
 
-    def forward_prop(self, input_, targets=None):
+    def forward_prop(self, input_, y_true=None):
         """
         If target is 'None', compute the probability of taking the correct path through the output graph.
         Else, compute the probability for each possible path (= each possible output class).
@@ -67,7 +67,7 @@ class HierarchicalSoftmax(object):
         batch_size = input_.shape[0]
 
         # compute all possible predictions [ time complexity is O(n_out) ]
-        if targets is None:
+        if y_true is None:
 
             def _path_probas(idx):
                 lev1_vec, lev2_vec = level1_vals[idx], level2_vals[idx]
@@ -76,17 +76,17 @@ class HierarchicalSoftmax(object):
                                               non_sequences=lev2_vec)
                 return result.flatten()
 
-            output_, updates = theano.scan(fn=_path_probas, sequences=T.arange(batch_size))
+            output, updates = theano.scan(fn=_path_probas, sequences=T.arange(batch_size))
 
             # since we may have more possible paths through the graph than output classes,
             # ignore the remaining paths
-            output_ = output_[:, :self.n_out]
+            output = output[:, :self.n_out]
 
         # compute only batch_size predictions [ time complexity is O(2 x sqrt(n_out)) = O(sqrt(n_out)) ]
         else:
             # to each class label, assign a pair of nodes in layer1 and layer2 of the output graph
-            level1_idx = targets // self.n_level1_nodes
-            level2_idx = targets % self.n_level2_nodes
+            level1_idx = y_true // self.n_level1_nodes
+            level2_idx = y_true % self.n_level2_nodes
 
             # calculate probability of taking correct path through the graph
             level1_val = level1_vals[T.arange(batch_size), level1_idx]
@@ -96,7 +96,7 @@ class HierarchicalSoftmax(object):
             # output is a matrix of predictions, with dimensionality (batch_size, n_out)
             # since we only have a probability for the correct label,
             #  we assign a probability of zero to all other labels
-            output_ = T.zeros((batch_size, self.n_out))
-            output_ = T.set_subtensor(output_[T.arange(batch_size), targets], target_probas)
+            output = T.zeros((batch_size, self.n_out))
+            output = T.set_subtensor(output[T.arange(batch_size), y_true], target_probas)
 
-        return output_
+        return output
