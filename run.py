9334d81f683cfc3b29584ef9bf3b9e7cd9adf9d7
RobGrimm <robert.grimm@uantwerpen.be>
2015-10-07 18:00:32 +0200

    cleanup

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from benchmark_functions import generate_data_train_softmax
 from benchmark_functions import benchmark_softmax
 
 # train hierarchical softmax on dataset with 5.000 output classes
-generate_data_train_softmax(n_classes=5000, n_training_examples=50000, input_size=3, hierarchical=True, n_epochs=2)
+generate_data_train_softmax(n_classes=5000, n_training_examples=50000, input_size=3, hierarchical=True, n_epochs=2,
+                            batch_size=32)
 
 
 # train flat softmax on the same data -- this will take longer
@@ -14,11 +15,11 @@ generate_data_train_softmax(n_classes=5000, n_training_examples=50000, input_siz
 generate_data_train_softmax(n_classes=1000000, n_training_examples=50000, input_size=3, hierarchical=True, n_epochs=2)
 
 
-# for varying numbers of output classes (500 -- 200.000, in increments of 500),
+# for varying numbers of output classes (1000 -- 20.000, in increments of 1000),
 # train both a flat and a hierarchical softmax model. keep track of the total
 # training time and the training loss at the last epoch for each model.
 # then plot cost and training time as a function of number of output classes, as well as the predicted class
 # for a randomly generated test example.
 # save plots to os.getcwd() + '\plots\'
-benchmark_softmax(n_classes_range=range(500, 20500, 500), plot_name='500--20k', input_size=100, n_data_points=50000,
+benchmark_softmax(n_classes_range=range(1000, 21000, 1000), plot_name='500--20k', input_size=100, n_data_points=500,
                   n_epochs=2)
